700261b569fdbd6c2f98ca95b3418ddf1bfeee08
Oii Woof <w15072362290@163.com>
2025-11-04 15:21:51 +0800

    feat(hash): 添加赎金信、同构字符串和单词规律解决方案- 实现赎金信问题的两种解法（HashMap 和数组） - 添加同构字符串判断逻辑 - 实现单词规律匹配功能 - 所有方法

---
+++ src/main/java/com/Oii/Problem150/HashSolution.java
@@ -0,0 +1,92 @@
+package com.Oii.Problem150;
+
+import java.util.HashMap;
+
+public class HashSolution {
+
+    // 25.11.04 - 1 赎金信 leetcode - 383
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) <= 0) {
+                    map.remove(c);
+                }
+            }
+        }
+
+        return map.isEmpty();
+    }
+
+    // 方法2 用数组代替，更快
+    public boolean canConstruct2(String ransomNote, String magazine) {
+        int[] map = new int[26];
+
+        for (int i = 0; i < magazine.length(); i++) {
+            map[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 'a';
+            if (map[index] == 0) {
+                return false;
+            }
+            map[index]--;
+        }
+        return true;
+    }
+
+    // 25.11.04 - 2 同构字符串 leetcode - 205
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (map.containsKey(c1)) {
+                if (map.get(c1) != c2) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(c2)) { // 值重复, a -> b, c -> b,所以这里检查是否值重复
+                    return false;
+                }
+                map.put(c1, c2);
+            }
+        }
+        return true;
+
+    }
+
+    // 25.11.04 - 3 单词规律 leetcode - 290
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map = new HashMap<>();
+
+        String[] words = s.split(" ");
+        if (words.length != pattern.length()) return false;
+
+        int i = 0;
+        for (String word : words) {
+            if (map.containsKey(pattern.charAt(i))) {
+                if (!map.get(pattern.charAt(i)).equals(word)) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(word)) {
+                    return false;
+                }
+                map.put(pattern.charAt(i), word);
+            }
+            i++;
+        }
+        return true;
+    }
+}
