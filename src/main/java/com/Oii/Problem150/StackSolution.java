cf672f56a720a62d87f427834447153706fbedd4
Oii Woof <w15072362290@163.com>
2025-11-11 23:25:07 +0800

    feat(hash): 添加哈希表相关算法实现
    - 实现有效的字母异位词判断方法
    - 添加字母异位词分组功能
    - 实现两数之和问题的哈希解法

--- src/main/java/com/Oii/Problem150/StackSolution.java
+++ src/main/java/com/Oii/Problem150/StackSolution.java
@@ -3,6 +3,7 @@ package com.Oii.Problem150;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
+
 public class StackSolution {
 
     // 25.11.10 - 1 有效的括号 leetcode - 20
@@ -61,4 +62,143 @@ public class StackSolution {
             return sb.toString();
         }
     }
+
+
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> numsStack = new LinkedList<>();
+
+        int res;
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    res = numsStack.pop() + numsStack.pop();
+                    numsStack.push(res);
+                    break;
+                case "-":
+                    res = -numsStack.pop() + numsStack.pop();
+                    numsStack.push(res);
+                    break;
+                case "*":
+                    res = numsStack.pop() * numsStack.pop();
+                    numsStack.push(res);
+                    break;
+                case "/":
+                    int a = numsStack.pop();
+                    int b = numsStack.pop();
+                    res = b / a;
+                    numsStack.push(res);
+                    break;
+                default:
+                    numsStack.push(Integer.parseInt(token));
+            }
+        }
+        return numsStack.pop();
+    }
+
+    public int calculate(String s) {
+        s = s.replace(" ", "");
+        Deque<Integer> stack = new LinkedList<>();
+        Deque<Character> opStack = new LinkedList<>();
+
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '(':
+                    opStack.push(c);
+                    break;
+                case ')':
+                    while (opStack.peekFirst() != '(') {
+                        Character op = opStack.pollFirst();
+                        switch (op) {
+                            case '+':
+                                res = stack.pop() + stack.pop();
+                                stack.push(res);
+                                break;
+                            case '-':
+                                res = -stack.pop() + stack.pop();
+                                stack.push(res);
+                                break;
+                            case '*':
+                                res = stack.pop() * stack.pop();
+                                stack.push(res);
+                                break;
+                            case '/':
+                                break;
+                            case null:
+                                break;
+                        }
+                    }
+                    opStack.pop();
+                default:
+                    stack.push(Integer.parseInt(String.valueOf(c)));
+            }
+            while (!opStack.isEmpty() && opStack.peekFirst() != '(') {
+                char op = opStack.pollFirst();
+                switch (op) {
+                    case '+':
+                        res = stack.pop() + stack.pop();
+                        stack.push(res);
+                        break;
+                    case '-':
+                        res = -stack.pop() + stack.pop();
+                        stack.push(res);
+                        break;
+                    case '*':
+                        res = stack.pop() * stack.pop();
+                        stack.push(res);
+                        break;
+                    case '/':
+                        int a = stack.pop();
+                        int b = stack.pop();
+                        res = b / a;
+                        stack.push(res);
+                        break;
+                }
+            }
+            return stack.pop();
+        }
+    }
 }
+
+class MinStack {
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        stack = new LinkedList<>();
+        minStack = new LinkedList<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            if (val <= minStack.peekFirst()) {
+                minStack.push(val);
+            }
+        }
+
+    }
+
+    public void pop() {
+        if (minStack.peekFirst() != null && minStack.peekFirst().equals(stack.peekFirst())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
