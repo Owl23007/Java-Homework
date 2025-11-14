3e5b2972b7b40632c32e4bd29d2816dc6fd07d50
Oii Woof <w15072362290@163.com>
2025-11-14 11:37:33 +0800

    feat(linkedlist): 实现链表相关算法题解
    - 添加环形链表检测算法
    - 实现两数相加链表版本- 实现合并两个有序链表的迭代与递归解法
    - 添加随机链表复制功能
    - 实现反转链表II指定区间反转功能
    - 优化逆波兰表达式求值算法注释- 增强基本计算器功能支持完整四则运算
    - 添加负数处理逻辑与操作符优先级判断
    - 完善表达式解析与数字提取工具方法- 添加JUnit依赖用于单元测试支持

---
+++ src/main/java/com/Oii/Problem150/LinkedListSolution.java
@@ -0,0 +1,168 @@
+package com.Oii.Problem150;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LinkedListSolution {
+    // 25.11.13 - 1 环形链表 leetcode - 141
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 25.11.13 - 2 两数相加 leetcode - 2
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0); // 虚拟头节点
+        ListNode current = res;
+        int needPlusOne = 0;  // 进位标志
+
+        // 当 l1 或 l2 不为空，或还有进位时继续循环
+        while (l1 != null || l2 != null || needPlusOne != 0) {
+            int sum = needPlusOne;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next; // 移动到下一位
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next; // 移动到下一位
+            }
+
+            needPlusOne = sum / 10; // 计算进位
+            current.next = new ListNode(sum % 10);
+            current = current.next; // 移动到结果链表下一位
+        }
+        return res.next; // 返回头节点
+    }
+
+    // 25.11.13 - 3 合并两个有序链表 leetcode - 21
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        ListNode res = new ListNode(0);
+
+        ListNode p = res;
+
+        while (p1 != null && p2!= null){
+            if (p1.val < p2.val){
+                p.next = p1;
+                p1 = p1.next;
+            }else{
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        p.next = p1 == null ? p2 : p1;
+        return res.next;
+    }
+
+    // 递归的解法
+    public ListNode mergeTwoLists_2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists_2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists_2(l1, l2.next);
+            return l2;
+        }
+    }
+
+    // 25.11.14 - 1 随机链表的复制 leetcode - 138
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public Node copyRandomList(Node head) {
+        Node res = new Node(0);
+        Node current = res;
+        Node p = head;
+
+        Map<Node, Node> map = new HashMap<>();
+        while (p != null) {
+            Node newNode = new Node(p.val);
+            current.next = newNode;
+            map.put(p, newNode);
+            current = current.next;
+            p = p.next;
+        }
+        p = head;
+        current = res.next;
+        while (p != null) {
+            if (p.random != null) {
+                current.random = map.get(p.random);
+            }
+
+            p = p.next;
+            current = current.next;
+        }
+
+        return res.next;
+    }
+
+    // 25.11.14 - 2 反转链表II leetcode - 92
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 创建一个虚拟头节点，简化边界情况的处理
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        // 移动到反转部分的前一个节点
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+
+        // start 是反转部分的第一个节点
+        ListNode start = pre.next;
+        // then 是 start 的下一个节点，将是反转过程中的关键节点
+        ListNode then = start.next;
+
+        // 执行 (right - left) 次操作来反转中间的节点
+        for (int i = 0; i < right - left; i++) {
+            start.next = then.next; // 将 then 节点从原来的位置断开
+            then.next = pre.next; // 将 then 节点连接到 pre 的后面，成为新的第一个节点
+            pre.next = then; // pre.next 指向这个新来的节点
+            then = start.next; // then 指向下一个待反转的节点
+        }
+        return dummy.next;
+    }
+}
--- src/main/java/com/Oii/Problem150/StackSolution.java
+++ src/main/java/com/Oii/Problem150/StackSolution.java
@@ -63,7 +63,7 @@ public class StackSolution {
         }
     }
 
-
+    // 25.11.12 - 1 逆波兰表达式求值 leetcode - 150
     public int evalRPN(String[] tokens) {
         Deque<Integer> numsStack = new LinkedList<>();
 
@@ -95,73 +95,149 @@ public class StackSolution {
         return numsStack.pop();
     }
 
+    // 25.11.12 - 2 基本计算器 leetcode - 224
+    // 原题虽然只是要求 0~9 ，且运算符只有加减、负数与括号
+    // 这里提供的是一个更通用的版本，可以处理用于整数加减乘除的表达式
     public int calculate(String s) {
-        s = s.replace(" ", "");
-        Deque<Integer> stack = new LinkedList<>();
-        Deque<Character> opStack = new LinkedList<>();
+        if (s == null || s.isEmpty()) return 0;
+        s = s.replaceAll(" ", ""); // 移除所有空格
 
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
+        Deque<Integer> values = new LinkedList<>(); // 数字栈
+        Deque<Character> ops = new LinkedList<>();  // 操作符栈
+
+        int i = 0;
+        boolean tag = true; // 标记上一个字符是否为'('或操作符后（可以出现负号）
+
+        while (i < s.length()) {
             char c = s.charAt(i);
-            switch (c) {
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '(':
-                    opStack.push(c);
-                    break;
-                case ')':
-                    while (opStack.peekFirst() != '(') {
-                        Character op = opStack.pollFirst();
-                        switch (op) {
-                            case '+':
-                                res = stack.pop() + stack.pop();
-                                stack.push(res);
-                                break;
-                            case '-':
-                                res = -stack.pop() + stack.pop();
-                                stack.push(res);
-                                break;
-                            case '*':
-                                res = stack.pop() * stack.pop();
-                                stack.push(res);
-                                break;
-                            case '/':
-                                break;
-                            case null:
-                                break;
-                        }
-                    }
-                    opStack.pop();
-                default:
-                    stack.push(Integer.parseInt(String.valueOf(c)));
-            }
-            while (!opStack.isEmpty() && opStack.peekFirst() != '(') {
-                char op = opStack.pollFirst();
-                switch (op) {
+
+            if (c == '-' && tag) {
+                // 负号处理：出现在开头或左括号后或操作符后
+                i++;
+                if (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    // 遇到数字，解析数字并压入取反操作符
+                    int num = parseNumber(s, i);
+                    i = skipDigits(s, i); // 更新索引到数字末尾
+                    values.push(num);
+                    ops.push('~');    // 压入取反操作符
+                    tag = false;
+                } else if (i < s.length() && s.charAt(i) == '(') {
+                    // 负号后面是左括号，压入取反操作符和左括号
+                    ops.push('~');
+                    ops.push('(');
+                    tag = true;
+                    i++;
+                } else {
+                    // 负号后不是数字也不是括号，按普通减法处理
+                    processOperator(values, ops, c);
+                    tag = true;
+                }
+            } else if (Character.isDigit(c)) {
+                // 解析完整数字
+                int num = parseNumber(s, i);
+                i = skipDigits(s, i); // 更新索引到数字末尾
+                values.push(num);
+                tag = false;
+            } else {
+                // 处理操作符和括号
+                switch (c) {
+                    case '(':
+                        ops.push(c);
+                        tag = true;
+                        i++;
+                        break;
+                    case ')':
+                        // 计算括号内的表达式
+                        processUntilLeftParen(values, ops);
+                        tag = false;
+                        i++;
+                        break;
                     case '+':
-                        res = stack.pop() + stack.pop();
-                        stack.push(res);
-                        break;
                     case '-':
-                        res = -stack.pop() + stack.pop();
-                        stack.push(res);
-                        break;
                     case '*':
-                        res = stack.pop() * stack.pop();
-                        stack.push(res);
-                        break;
                     case '/':
-                        int a = stack.pop();
-                        int b = stack.pop();
-                        res = b / a;
-                        stack.push(res);
+                        processOperator(values, ops, c);
+                        tag = true;
+                        i++;
+                        break;
+                    default:
+                        // 跳过其他字符
+                        i++;
                         break;
                 }
             }
-            return stack.pop();
         }
+
+        // 处理栈中剩余的所有操作
+        while (!ops.isEmpty()) {
+            processOperatorLeft(values, ops);
+        }
+
+        return values.pop();
+    }
+
+    private int parseNumber(String s, int start) {
+        int num = 0;
+        int i = start;
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            num = num * 10 + (s.charAt(i) - '0');
+            i++;
+        }
+        return num;
+    }
+
+    private int skipDigits(String s, int start) {
+        int i = start;
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            i++;
+        }
+        return i;
+    }
+
+    private void processUntilLeftParen(Deque<Integer> values, Deque<Character> ops) {
+        while (!ops.isEmpty() && ops.peek() != '(') {
+            processOperatorLeft(values, ops);
+        }
+        if (!ops.isEmpty()) {
+            ops.pop(); // 弹出 '('
+        }
+    }
+
+    private void processOperator(Deque<Integer> values, Deque<Character> ops, char c) {
+        while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(c)) {
+            processOperatorLeft(values, ops);
+        }
+        ops.push(c);
+    }
+
+    private void processOperatorLeft(Deque<Integer> values, Deque<Character> ops) {
+        char op = ops.pop();
+        if (op == '~') {
+            values.push(-values.pop());
+        } else {
+            int val2 = values.pop();
+            int val1 = values.pop();
+            values.push(applyOp(val1, val2, op));
+        }
+    }
+
+    // 定义操作符优先级
+    private int precedence(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        if (op == '~') return 3; // 取反操作符优先级最高
+        return 0; // '(' 的优先级最低
+    }
+
+    // 执行单个运算
+    private int applyOp(int a, int b, char op) {
+        return switch (op) {
+            case '+' -> a + b;
+            case '-' -> a - b;
+            case '*' -> a * b;
+            case '/' -> a / b;
+            default -> 0;
+        };
     }
 }
 
